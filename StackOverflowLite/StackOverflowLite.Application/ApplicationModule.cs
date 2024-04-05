a31a077e3da6c760b4aec4f6c3c806e5b4412bc4
Sharif03 <abinsu7@gmail.com>
2024-04-05 23:14:58 +0600

    Three(Domain, Application, Infrastructure) extra modules are created to implement DDD & Clean Architecture. And add Autofac & Serilog into each three projects.

---
+++ StackOverflowLite/StackOverflowLite.Application/ApplicationModule.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace StackOverflowLite.Application
+{
+    public class ApplicationModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            // builder.RegisterType<>().As<>();
+        }
+    }
+}
---
+++ StackOverflowLite/StackOverflowLite.Web/WebModule.cs
@@ -0,0 +1,11 @@
+﻿using Autofac;
+namespace StackOverflowLite.Web
+{
+    public class WebModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            // builder.RegisterType<>().As<>();
+        }
+    }
+}
