b459f941caf950c143cfbac2637015d91aecfa47
Sharif03 <abinsu7@gmail.com>
2024-04-15 10:10:24 +0600

    User entity modified

--- StackOverflowLite/StackOverflowLite.Domain/Entities/User.cs
+++ StackOverflowLite/StackOverflowLite.Domain/Entities/User.cs
@@ -15,11 +15,8 @@ namespace StackOverflowLite.Domain.Entities
     public class User : IEntity<Guid>
     {
         public Guid Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
         public bool IsVerified { get; private set; }
         public Level UserLevel { get; set; }
         public List<Question> Question { get; set; }
