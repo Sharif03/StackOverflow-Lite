1939a657235f239a0ebbc50d8ad3f622794aec5c
Sharif03 <abinsu7@gmail.com>
2024-04-18 08:42:58 +0600

    Answer entity added & Question entity modified to fix for the project requirement

--- StackOverflowLite/StackOverflowLite.Domain/Entities/Answer.cs
+++ StackOverflowLite/StackOverflowLite.Domain/Entities/Answer.cs
@@ -3,22 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace StackOverflowLite.Domain.Entities
 {
-    public class Question : IEntity<Guid>
+    public class Answer
     {
         public Guid Id { get; set; }
-        public string Title { get; set; }
         public string Content { get; set; }
-        public string Tags { get; set; }
-        public int Upvote { get; set; }
-        public int Downvote { get; set; }
         public DateTime QuestionPosted { get; set; }
         public Guid UserId { get; set; }
         public User User { get; set; }
-        public IList<Comment> Comments { get; set; }
-        public IList<Answer> Answers { get; set; }
+        public Guid QuestionId { get; set; }
+        public Question Question { get; set; }
     }
 }
