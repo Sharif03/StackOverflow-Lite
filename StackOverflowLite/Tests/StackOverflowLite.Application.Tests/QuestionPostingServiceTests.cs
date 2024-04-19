08b407145d1fbed245f0f064c9dd339e3ccbc04a
Sharif03 <abinsu7@gmail.com>
2024-04-19 15:30:13 +0600

    Setup ready for QuestionPostingServiceTests. AutoMock Apply for simplifies the creation and management of mock objects for unit testing.

---
+++ StackOverflowLite/Tests/StackOverflowLite.Application.Tests/QuestionPostingServiceTests.cs
@@ -0,0 +1,49 @@
+using Autofac.Extras.Moq;
+using Moq;
+using StackOverflowLite.Application.Features.Posting.Services;
+using StackOverflowLite.Domain.Repositories;
+
+namespace StackOverflowLite.Application.Tests
+{
+    public class QuestionPostingServiceTests
+    {
+        private AutoMock _mock;
+        private Mock<IQuestionRepository> _questionRepositoryMock;
+        private Mock<IApplicationUnitOfWork> _unitOfWorkMock;
+        private Mock<IUserIdentityService> _userIdentityServiceMock;
+        private QuestionPostingService _questionPostingService;
+  
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _mock = AutoMock.GetLoose();
+        }
+        [SetUp]
+        public void Setup()
+        {
+            _questionRepositoryMock = _mock.Mock<IQuestionRepository>();
+            _unitOfWorkMock = _mock.Mock<IApplicationUnitOfWork>();
+            _userIdentityServiceMock = _mock.Mock<IUserIdentityService>();
+            _questionPostingService = _mock.Create<QuestionPostingService>();
+        }
+        [TearDown]
+        public void Teardown()
+        {
+            _questionRepositoryMock?.Reset();
+            _unitOfWorkMock?.Reset();
+            _userIdentityServiceMock?.Reset();
+        }
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            _mock?.Dispose();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
