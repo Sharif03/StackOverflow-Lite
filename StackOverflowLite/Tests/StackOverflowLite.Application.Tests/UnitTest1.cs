57e98e8423b269a80835d025002059e15fe7f7e9
Sharif03 <abinsu7@gmail.com>
2024-04-19 12:09:23 +0600

    Unit Testing-NUnit Framework added to test codes behavior.

---
+++ StackOverflowLite/Tests/StackOverflowLite.Application.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace StackOverflowLite.Application.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
